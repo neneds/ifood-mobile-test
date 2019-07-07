04076410c394acd2be9c8d2fb00e9022b3fafa18
Dennis Merli <neneds@gmail.com>
2019-07-07 16:25:58 -0300

    creating uikit

--- twitter-mood/twitter-mood/Resources/UIKit/UIKit.swift
+++ twitter-mood/twitter-mood/Resources/UIKit/UIKit.swift
@@ -11,16 +11,12 @@ import UIKit
 struct UIKit {
     
     // MARK: Colors
-    ///#64428C
-    static var purple64428C: UIColor { return UIColor.fromHex(0x64428C) }
-    ///#8468A2
-    static var purple8468A2: UIColor { return UIColor.fromHex(0x64428C) }
-    ///#E2643F
-    static var orangeE2643F: UIColor { return UIColor.fromHex(0x64428C) }
-    ///#E4E4E4
-    static var greyE4E4E4: UIColor { return UIColor.fromHex(0x64428C) }
     
-    
+    ///#323232
+    static var grey323232: UIColor { return UIColor.fromHex(0x323232) }
+    ///#1B1B1B
+    static var grey1B1B1B: UIColor { return UIColor.fromHex(0x1B1B1B) }
+
     // MARK: Fonts Styles
     static var smallText1: UIFont {
         return UIFont.systemFont(ofSize: 11, weight: .regular)
--- twitter-mood/twitter-mood/Sources/Flows/TwitterUserLookup/ViewController/TwitterUserLookupViewController.swift
+++ twitter-mood/twitter-mood/Sources/Flows/TwitterUserLookup/ViewController/TwitterUserLookupViewController.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 class TwitterUserLookupViewController: BaseViewController {
-
+    
+    @IBOutlet private weak var labelTwitterUserTitle: UILabel!
+    @IBOutlet private weak var textFieldInput: UITextField!
+    @IBOutlet private weak var buttonSeeTweets: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func setLocalizations() {
+        labelTwitterUserTitle.text = TwitterUserLookupLocalizable.twitterUser
+        buttonSeeTweets.setTitle(TwitterUserLookupLocalizable.seeTweets, for: .normal)
+        textFieldInput.placeholder = TwitterUserLookupLocalizable.twitterUserName
+        
     }
-    */
 
 }
